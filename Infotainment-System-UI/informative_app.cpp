8d059ce278bc94ef209ea2cd5bdd5347b1ffe340
ali ahmed <aliahmed999416@gmail.com>
2024-05-01 22:00:54 +0300

    adding comments

--- Infotainment-System-UI/informative_app.cpp
+++ Infotainment-System-UI/informative_app.cpp
@@ -7,7 +7,7 @@
 #include<QPixmap>
 
 
-QFrame *line3;
+
 Informative_APP::Informative_APP(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Informative_APP)
@@ -17,8 +17,11 @@ Informative_APP::Informative_APP(QWidget *parent)
     // Initializing process
     process = new QProcess(this);
 
+    // connect the information button to add_label and kernel_info functions
+    // add_label and kernel_info functions invoked when information button pressed
     connect(ui->information, &QPushButton::clicked,this,&Informative_APP::add_label);
     connect(ui->information, &QPushButton::clicked,this,&Informative_APP::Kernel_info);
+    // connect dateTime button to remove_label function
     connect(ui->dateTime, &QPushButton::clicked,this,&Informative_APP::remove_label);
 }
 
@@ -30,26 +33,35 @@ Informative_APP::~Informative_APP()
 
 void Informative_APP::add_label()
 {
+    // intializing a pointer points to frame layout  so added labels will have the same layout
     QVBoxLayout* layout = qobject_cast<QVBoxLayout*>(ui->frame->layout());
 
 
+    // to clear the screen every time the buttton pressed
+    // de-allocate pointers
     delete Name;
     delete line;
 
+    // reintiallize the pointer to access the frame that the labels and lines will be inserted
     Name = new QLabel(ui->frame);
     line = new QFrame(ui->frame);
 
-    Name->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
-    //Name->setAlignment(Qt::AlignLeft | Qt::AlignHCenter);
-    Name->setText("<span style='color: rgb(246, 245, 244); font-family:  CCS Neue Rinjani, Times, serif; font-weight:bold; \
+
+    // <span> tag --> used to give each line a different style
+    //<span> dispalyed word </span>
+    Name->setText("<span style='color: rgb(246, 245, 244); font-family:  Times New Roman, Times, serif; font-weight:bold; \
                         font-size: 24pt;font-stretch:ultra-condensed;'>Name</span><br>\
                     <span style='color: rgb(246, 245, 244);font-family:  Times New Roman, Times,sans-serif; font-weight:bold; \
                          font-size: 16pt;'>Sudo Team</span>");
 
+    // set frame shape to Horizontal line
     line->setFrameShape(QFrame::HLine);
+    // set the shadow to sunked"
     line->setFrameShadow(QFrame::Sunken);
+    // set frame hieght to 2pt
     line->setFixedHeight(2);
-    line->setStyleSheet("background-color: #302A43"); // Set line color
+    // Set line color
+    line->setStyleSheet("background-color: #302A43");
 
 
     // shows the label on the window
@@ -64,15 +76,16 @@ void Informative_APP::Kernel_info()
 
     QVBoxLayout* layout = qobject_cast<QVBoxLayout*>(ui->frame->layout());
 
+    // to clear the screen every time the buttton pressed
+    // de-allocate pointers
     delete kernel_version;
     delete kernel_update;
     delete line2;
     delete line3;
 
+    // reintiallize the pointers to access the frame that the labels and lines will be inserted
     kernel_version = new QLabel(ui->frame);
     kernel_update = new QLabel(ui->frame);
-
-
     line2 = new QFrame(ui->frame);
     line3 = new QFrame(ui->frame);
 
@@ -88,31 +101,46 @@ void Informative_APP::Kernel_info()
     // Get the path of the kernel image file
     QString kernelPath = "/boot/vmlinuz-" + kernelVersion;
 
-    // Check if the kernel image file exists
+    // extract kernel information from the current kernel version
     QFileInfo kernelInfo(kernelPath);
+    // extract the last time kernel have been updated
     QDateTime modificationDate = kernelInfo.lastModified();
+    // convert date extracted to a string
     QString dateModified = modificationDate.toString();
 
-
+    // <span> tag --> used to give each line a different style
+    //<span> dispalyed word </span>
+    // + kernelVersion  + --> used to print content of the vriable"kernel version"
     kernel_version->setText("<span style='color: rgb(246, 245, 244);font-family:  Times New Roman, Times, serif; font-weight:bold; \
                             font-size: 24pt;'>Version</span><br>"\
                         "<span style='color: rgb(246, 245, 244);font-family:  Times New Roman, Times,sans-serif; font-weight:bold; \
                              font-size: 16pt;'>"+ kernelVersion +"</span>");
 
+    // <span> tag --> used to give each line a different style
+    //<span> dispalyed word </span>
+    // + dateModified  + --> used to print content of the vriable "last update time"
     kernel_update->setText("<span style='color: rgb(246, 245, 244);font-family:  Times New Roman, Times, serif; font-weight:bold; \
             font-size: 24pt;'>Last Update</span><br>"\
         "<span style='color: rgb(246, 245, 244);font-family:  Times New Roman, Times,sans-serif; font-weight:bold; \
              font-size: 16pt;'>"+ dateModified +"</span>");
 
+    // set frame shape to Horizontal line
     line2->setFrameShape(QFrame::HLine);
+    // set the shadow to sunked"
     line2->setFrameShadow(QFrame::Sunken);
+    // set frame hieght to 2pt
     line2->setFixedHeight(2);
-    line2->setStyleSheet("background-color: #302A43"); // Set line color
+    // Set line color
+    line2->setStyleSheet("background-color: #302A43");
 
+    // set frame shape to Horizontal line
     line3->setFrameShape(QFrame::HLine);
+    // set the shadow to sunked"
     line3->setFrameShadow(QFrame::Sunken);
+    // set frame hieght to 2pt
     line3->setFixedHeight(2);
-    line3->setStyleSheet("background-color: #302A43"); // Set line color
+    // Set line color
+    line3->setStyleSheet("background-color: #302A43");
 
 
     // shows the label on the window
@@ -126,6 +154,7 @@ void Informative_APP::Kernel_info()
 
 void Informative_APP::remove_label()
 {
+    // hide the labels and lines when another screen is pressed
     Name->hide();
     kernel_version->hide();
     kernel_update->hide();
--- Infotainment-System-UI/informative_app.h
+++ Infotainment-System-UI/informative_app.h
@@ -19,18 +19,29 @@ public:
     Informative_APP(QWidget *parent = nullptr);
     ~Informative_APP();
 
+    // function to add labels when button pressed
     void add_label();
+    // function to remove labels when button pressed
     void remove_label();
+    // function to get kernel information
+    // display these information in a label when button pressed
     void Kernel_info();
 
 private:
     Ui::Informative_APP *ui;
+
+    // intializing a pointer to process to start a new process
     QProcess *process;
-    QLabel *Name;
-    QLabel *kernel_version;
-    QLabel *kernel_update;
-    QFrame *line;
-    QFrame *line2;
+
+    // allocating pointers make labels
+    QLabel *Name = new QLabel();
+    QLabel *kernel_version = new QLabel();
+    QLabel *kernel_update = new QLabel();
+
+    // allocating pointers make lines
+    QFrame *line = new QFrame();
+    QFrame *line2 = new QFrame();
+    QFrame *line3 = new QFrame();
 
 
 };
