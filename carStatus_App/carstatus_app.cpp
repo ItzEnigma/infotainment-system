85252c8e2a0dacab6518f66c420a44be7393fbfa
ali ahmed <aliahmed999416@gmail.com>
2024-05-06 16:48:13 +0300

    adding leds

--- carStatus_App/carstatus_app.cpp
+++ carStatus_App/carstatus_app.cpp
@@ -4,7 +4,7 @@
 #include<QBoxLayout>
 #include<QFile>
 #include<QTimer>
-#include<QDebug>
+
 
 
 
@@ -36,7 +36,7 @@ void carStatus_App::add_Gimg()
     green_temp->setSizeIncrement(ui->frame->width(),ui->frame->height());
     layout->addWidget(green_temp);
 
-    QPixmap temp(":/img/img/car.png");
+    QPixmap temp(":/img/img/blue_temp.png");
     int w = ui->frame->width()-20;
     int h = ui->frame->height()-20;
     green_temp->setPixmap(temp.scaled(w,h,Qt::KeepAspectRatio));
@@ -56,7 +56,7 @@ void carStatus_App::add_Rimg()
     red_temp->setSizeIncrement(ui->frame->width(),ui->frame->height());
     layout->addWidget(red_temp);
 
-    QPixmap temp(":/img/thermometer.png");
+    QPixmap temp(":/img/img/red_temp.png");
     int w = ui->frame->width()-20;
     int h = ui->frame->height()-20;
     red_temp->setPixmap(temp.scaled(w,h,Qt::KeepAspectRatio));
@@ -71,9 +71,64 @@ void carStatus_App::remove_img()
 }
 
 
+
+void carStatus_App::add_Bled()
+{
+
+    QVBoxLayout *layout = new QVBoxLayout(ui->LED1_Icon);
+
+    blue_led->hide();
+    delete blue_led;
+    blue_led = new QLabel(ui->LED1_Icon);
+    blue_led->setAlignment(Qt::AlignCenter);
+    blue_led->setSizeIncrement(ui->LED1_Icon->width(),ui->LED1_Icon->height());
+    layout->addWidget(blue_led);
+
+    QPixmap temp(":/img/img/blue led.png");
+    int w = ui->LED1_Icon->width();
+    int h = ui->LED1_Icon->height();
+    blue_led->setPixmap(temp.scaled(w,h,Qt::KeepAspectRatio));
+
+    delete layout;
+}
+
+void carStatus_App::add_Rled()
+{
+
+    QVBoxLayout *layout = new QVBoxLayout(ui->LED2_Icon);
+
+    red_led->hide();
+    delete red_led;
+    red_led = new QLabel(ui->LED2_Icon);
+    red_led->setAlignment(Qt::AlignCenter);
+    red_led->setSizeIncrement(ui->LED2_Icon->width(),ui->LED2_Icon->height());
+    layout->addWidget(red_led);
+
+    QPixmap temp(":/img/img/red led.png");
+    int w = ui->LED2_Icon->width();
+    int h = ui->LED2_Icon->height();
+    red_led->setPixmap(temp.scaled(w,h,Qt::KeepAspectRatio));
+
+    delete layout;
+}
+
+void carStatus_App::remove_leds()
+{
+    blue_led->hide();
+    red_led->hide();
+
+}
+
+
 void carStatus_App::read_temp()
 {
-    QFile file("/sys/class/leds/input3::capslock/brightness");
+   // variable to store the temperature
+    int temp;
+
+    //set the path to the file
+    QFile file("/sys/class/leds/input2::capslock/brightness");
+
+    // open tthe file to read
     file.open(QIODevice::ReadOnly);
 
     // Read the contents of the file
@@ -83,12 +138,25 @@ void carStatus_App::read_temp()
     QString text = QString::fromUtf8(data);
     // removes the "\n" from the end of the string
     text.replace("\n"," ");
+
+
+
+
     // display the output on the plainText
+    // QChar( 0x00B0) --> to display the temerature degree
     ui->label->setText("<span style='color: rgb(246, 245, 244); font-family:  Times New Roman, Times, serif; font-weight:bold; \
                        font-size: 28pt;font-stretch:ultra-condensed;'>"+ text +QChar( 0x00B0) +"C" );
+    // closing the file
     file.close();
-    text.remove(" ");
-    if (text == "0")
+
+
+    //  convert  the readint into integer value
+    temp = text.toInt();
+
+
+    add_Bled();
+    add_Rled();
+    if (temp == 0)
     {
         carStatus_App::remove_img();
         carStatus_App::add_Gimg();
@@ -99,4 +167,6 @@ void carStatus_App::read_temp()
         carStatus_App::add_Rimg();
     }
 }
+
+
 
--- carStatus_App/carstatus_app.h
+++ carStatus_App/carstatus_app.h
@@ -22,11 +22,20 @@ public:
     void add_Gimg();
     void add_Rimg();
     void remove_img();
+
+    void add_Bled();
+    void add_Rled();
+    void remove_leds();
+
     void read_temp();
+
 
 private:
     Ui::carStatus_App *ui;
     QLabel *green_temp = new QLabel();
     QLabel *red_temp = new QLabel();
+
+    QLabel *blue_led = new QLabel();
+    QLabel *red_led = new QLabel();
 };
 #endif // CARSTATUS_APP_H
