6c66c93466ad3dc54496999dccb030ce77347cb6
ItzEnigma <hosamhosam01@gmail.com>
2024-05-07 00:24:22 +0300

    Adding exception handling

--- carStatus_App/carstatus_app.cpp
+++ carStatus_App/carstatus_app.cpp
@@ -1,23 +1,18 @@
 #include "carstatus_app.h"
 #include "ui_carstatus_app.h"
-#include<QWidget>
-#include<QBoxLayout>
-#include<QFile>
-#include<QTimer>
-
-
-
+#include <QWidget>
+#include <QBoxLayout>
+#include <QFile>
+#include <QTimer>
 
 carStatus_App::carStatus_App(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::carStatus_App)
+    : QMainWindow(parent), ui(new Ui::carStatus_App)
 {
     ui->setupUi(this);
 
-
     QTimer *timer = new QTimer(this);
     // invokes read_temp function when the timer timees out
-    connect(timer,&QTimer::timeout,this,&carStatus_App::read_temp);
+    connect(timer, &QTimer::timeout, this, &carStatus_App::read_temp);
     // timer times out every 100ms
     timer->start(100);
 }
@@ -27,30 +22,29 @@ carStatus_App::~carStatus_App()
     delete ui;
 }
 
-
 /* function that creates a new label and add the BLUE temperature indictor to the screen*/
 void carStatus_App::add_Gimg()
 {
     // Intializes a vertical layout to the temperature frame
     QVBoxLayout *layout = new QVBoxLayout(ui->temp_frame);
 
-   /*delete the image when the function starts to save memory */
+    /*delete the image when the function starts to save memory */
     green_temp->hide();
     delete green_temp;
     // creating new label in the temperature frame...so the image set in it
     green_temp = new QLabel(ui->temp_frame);
-    //Align the label content to the center
+    // Align the label content to the center
     green_temp->setAlignment(Qt::AlignCenter);
     // set the size of the label to the size  of the temerature frame
-    green_temp->setSizeIncrement(ui->temp_frame->width(),ui->temp_frame->height());
+    green_temp->setSizeIncrement(ui->temp_frame->width(), ui->temp_frame->height());
 
     // set the path of the blue image to temp variable
     QPixmap temp(":/img/img/blue_temp.png");
     // set the the hieght and width of the image to the hieght and width of the frame
     // KeepAspectRation -->  to show the full  image
-    int w = ui->temp_frame->width()-20;
-    int h = ui->temp_frame->height()-20;
-    green_temp->setPixmap(temp.scaled(w,h,Qt::KeepAspectRatio));
+    int w = ui->temp_frame->width() - 20;
+    int h = ui->temp_frame->height() - 20;
+    green_temp->setPixmap(temp.scaled(w, h, Qt::KeepAspectRatio));
 
     // dispaly the label with the blue temperature indicator to the temperature frame
     layout->addWidget(green_temp);
@@ -71,18 +65,18 @@ void carStatus_App::add_Rimg()
     delete red_temp;
     // creating new label in the temperature frame...so the image set in it
     red_temp = new QLabel(ui->temp_frame);
-    //Align the label content to the center
+    // Align the label content to the center
     red_temp->setAlignment(Qt::AlignCenter);
     // set the size of the label to the size  of the temerature frame
-    red_temp->setSizeIncrement(ui->temp_frame->width(),ui->temp_frame->height());
+    red_temp->setSizeIncrement(ui->temp_frame->width(), ui->temp_frame->height());
 
     // set the path of the red image to temp variable
     QPixmap temp(":/img/img/red_temp.png");
-    int w = ui->temp_frame->width()-20;
-    int h = ui->temp_frame->height()-20;
+    int w = ui->temp_frame->width() - 20;
+    int h = ui->temp_frame->height() - 20;
     // set the the hieght and width of the image to the hieght and width of the frame
     // KeepAspectRation -->  to show the full  image
-    red_temp->setPixmap(temp.scaled(w,h,Qt::KeepAspectRatio));
+    red_temp->setPixmap(temp.scaled(w, h, Qt::KeepAspectRatio));
 
     // dispaly the label with the red temperature indicator to the temperature frame
     layout->addWidget(red_temp);
@@ -91,14 +85,12 @@ void carStatus_App::add_Rimg()
     delete layout;
 }
 
-
 /* function that removes the temerature indicators */
 void carStatus_App::remove_img()
 {
     green_temp->hide();
     red_temp->hide();
 }
-
 
 /* funtction that adds the blue led */
 void carStatus_App::add_Bled_on()
@@ -113,11 +105,11 @@ void carStatus_App::add_Bled_on()
 
     // creating new label in the red led frame...so the image set in it
     blue_led = new QLabel(ui->blue_led_frame);
-    //Align the label content to the center
+    // Align the label content to the center
     blue_led->setAlignment(Qt::AlignCenter);
     // set the size of the label to the size  of the blue led frame
-    blue_led->setSizeIncrement(ui->blue_led_frame->width(),ui->blue_led_frame->height());
-	
+    blue_led->setSizeIncrement(ui->blue_led_frame->width(), ui->blue_led_frame->height());
+
     // dispaly the label with the blue led to the blue led frame
     layout->addWidget(blue_led);
 
@@ -127,8 +119,7 @@ void carStatus_App::add_Bled_on()
     // KeepAspectRation -->  to show the full  image
     int w = ui->blue_led_frame->width();
     int h = ui->blue_led_frame->height();
-    blue_led->setPixmap(temp.scaled(w,h,Qt::KeepAspectRatio));
-
+    blue_led->setPixmap(temp.scaled(w, h, Qt::KeepAspectRatio));
 
     delete layout;
 }
@@ -145,10 +136,10 @@ void carStatus_App::add_Rled_on()
 
     // creating new label in the red led frame...so the image set in it
     red_led = new QLabel(ui->red_led_frame);
-    //Align the label content to the center
+    // Align the label content to the center
     red_led->setAlignment(Qt::AlignCenter);
     // set the size of the label to the size  of the red led frame
-    red_led->setSizeIncrement(ui->red_led_frame->width(),ui->red_led_frame->height());
+    red_led->setSizeIncrement(ui->red_led_frame->width(), ui->red_led_frame->height());
 
     // dispaly the label with the red temperature indicator to the temperature frame
     layout->addWidget(red_led);
@@ -159,13 +150,11 @@ void carStatus_App::add_Rled_on()
     // KeepAspectRation -->  to show the full  image
     int w = ui->red_led_frame->width();
     int h = ui->red_led_frame->height();
-    red_led->setPixmap(temp.scaled(w,h,Qt::KeepAspectRatio));
-
+    red_led->setPixmap(temp.scaled(w, h, Qt::KeepAspectRatio));
 
     // delete the layout...so it can be remade
     delete layout;
 }
-
 
 void carStatus_App::add_Bled_off()
 {
@@ -179,21 +168,21 @@ void carStatus_App::add_Bled_off()
 
     // creating new label in the red led frame...so the image set in it
     blue_led_off = new QLabel(ui->blue_led_frame);
-    //Align the label content to the center
+    // Align the label content to the center
     blue_led_off->setAlignment(Qt::AlignCenter);
     // set the size of the label to the size  of the blue led frame
-    blue_led_off->setSizeIncrement(ui->blue_led_frame->width(),ui->blue_led_frame->height());
+    blue_led_off->setSizeIncrement(ui->blue_led_frame->width(), ui->blue_led_frame->height());
 
     // dispaly the label with the blue led to the blue led frame
     layout->addWidget(blue_led_off);
-    
+
     // set the path of the blue image to blue led variable
     QPixmap temp(":/img/img/blue led off.png");
     // set the the hieght and width of the image to the hieght and width of the frame
     // KeepAspectRation -->  to show the full  image
     int w = ui->blue_led_frame->width();
     int h = ui->blue_led_frame->height();
-    blue_led_off->setPixmap(temp.scaled(w,h,Qt::KeepAspectRatio));
+    blue_led_off->setPixmap(temp.scaled(w, h, Qt::KeepAspectRatio));
 
     // dispaly the label with the blue led to the blue led frame
     layout->addWidget(blue_led_off);
@@ -213,10 +202,10 @@ void carStatus_App::add_Rled_off()
 
     // creating new label in the red led frame...so the image set in it
     red_led_off = new QLabel(ui->red_led_frame);
-    //Align the label content to the center
+    // Align the label content to the center
     red_led_off->setAlignment(Qt::AlignCenter);
     // set the size of the label to the size  of the red led frame
-    red_led_off->setSizeIncrement(ui->red_led_frame->width(),ui->red_led_frame->height());
+    red_led_off->setSizeIncrement(ui->red_led_frame->width(), ui->red_led_frame->height());
 
     // dispaly the label with the red temperature indicator to the temperature frame
     layout->addWidget(red_led_off);
@@ -227,8 +216,7 @@ void carStatus_App::add_Rled_off()
     // KeepAspectRation -->  to show the full  image
     int w = ui->red_led_frame->width();
     int h = ui->red_led_frame->height();
-    red_led_off->setPixmap(temp.scaled(w,h,Qt::KeepAspectRatio));
-
+    red_led_off->setPixmap(temp.scaled(w, h, Qt::KeepAspectRatio));
 
     // delete the layout...so it can be remade
     delete layout;
@@ -244,40 +232,51 @@ void carStatus_App::remove_leds()
 
 void carStatus_App::read_temp()
 {
-   // variable to store the temperature
+    // variable to store the temperature
     int temp;
 
-    //set the path to the file
-    QFile file("/sys/class/leds/input2::capslock/brightness");
+    /* set the path to the file */
+    /* ----- MODIFY THE PATH TO THE TEMPERATURE FILE ----- */
+    QString path = "/sys/class/leds/input3::capslock/brightness";
+    /* -------------------------------------------------- */
 
-    // open tthe file to read
-    file.open(QIODevice::ReadOnly);
+    try
+    {
+        // check if the file exists
+        if (!QFile::exists(path))
+        {
+            throw 1;
+        }
 
-    // Read the contents of the file
-    QByteArray data = file.readAll();
+        // open the file
+        QFile file(path);
 
-    // Convert data to string
-    QString text = QString::fromUtf8(data);
-    // removes the "\n" from the end of the string
-    text.replace("\n"," ");
+        // open the file to read
+        file.open(QIODevice::ReadOnly);
 
+        // Read the contents of the file
+        QByteArray data = file.readAll();
 
+        // Convert data to string
+        QString text = QString::fromUtf8(data);
+        // removes the "\n" from the end of the string
+        text.replace("\n", " ");
 
+        // display the output on the plainText
+        // QChar( 0x00B0) --> to display the temerature degree
+        ui->label->setText("<span style='color: rgb(246, 245, 244); font-family:  Times New Roman, Times, serif; font-weight:bold; \
+                       font-size: 36pt;font-stretch:ultra-condensed;'>" +
+                           text + QChar(0x00B0) + "C");
+        // closing the file
+        file.close();
 
-    // display the output on the plainText
-    // QChar( 0x00B0) --> to display the temerature degree
-    ui->label->setText("<span style='color: rgb(246, 245, 244); font-family:  Times New Roman, Times, serif; font-weight:bold; \
-                       font-size: 36pt;font-stretch:ultra-condensed;'>"+ text +QChar( 0x00B0) +"C" );
-    // closing the file
-    file.close();
-
-
-    //  convert  the readint into integer value
-    temp = text.toInt();
-
-
-
-
+        //  convert  the readint into integer value
+        temp = text.toInt();
+    }
+    catch (int e) // if the file does not exist
+    {
+        temp = 0;
+    }
 
     /* CHECK IF THE TEMPERATURE IS HIGH OR LOW*/
     if (temp == 0)
@@ -297,5 +296,3 @@ void carStatus_App::read_temp()
         add_Rled_on();
     }
 }
-
-
